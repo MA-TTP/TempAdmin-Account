37f347f1bc541c4b0415cafd56e8836145c2c470
MA-TTP <72108630+MA-TTP@users.noreply.github.com>
2022-10-18 09:01:26 -0400

    Update TempAdminInt.bat
    Signed-off-by: MA-TTP <72108630+MA-TTP@users.noreply.github.com>

--- TempAdminInt.bat
+++ TempAdminInt.bat
@@ -1,4 +1,4 @@
-@title TempAdmin internal. GitHub Edition. License: CC0 1.0 Universal
+@title TempAdmin internal. GitHub Edition. License: MIT
 @echo  Copyright Mashoor Aiyaan. Version 1.0, build 101722. This script is to be used in another script.
 ::edit out "TA" with any password you like, such as 1234.
 @net user TempAdmin TA /add
