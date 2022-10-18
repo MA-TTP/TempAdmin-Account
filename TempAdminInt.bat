df193ebb3931a99c8e120ee70c8169789b3767a8
MA-TTP <72108630+MA-TTP@users.noreply.github.com>
2022-10-18 08:47:33 -0400

    Update TempAdminInt.bat
    Signed-off-by: MA-TTP <72108630+MA-TTP@users.noreply.github.com>

--- TempAdminInt.bat
+++ TempAdminInt.bat
@@ -1,8 +1,8 @@
-@title QuickAdmin internal. GitHub Edition. License: CC0 1.0 Universal
+@title TempAdmin internal. GitHub Edition. License: CC0 1.0 Universal
 @echo  Copyright Mashoor Aiyaan. Version 1.0, build 101722. This script is to be used in another script.
-::edit out "QA" with any password you like, such as 1234.
-@net user QuickAdmin QA /add
-@net localgroup Administrators QuickAdmin /add
+::edit out "TA" with any password you like, such as 1234.
+@net user TempAdmin TA /add
+@net localgroup Administrators TempAdmin /add
 @echo Press any key once you are done with tasks. The account auto deletes in 10 minutes.
 ::If you want, change 600 to a custom time period, in seconds.
 @timeout 600
