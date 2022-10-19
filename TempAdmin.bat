c966af90ecdf1b143376f5ee6c85d33009503ff3
MA-TTP <72108630+MA-TTP@users.noreply.github.com>
2022-10-19 08:53:45 -0400

    Update TempAdmin.bat

--- TempAdmin.bat
+++ TempAdmin.bat
@@ -4,7 +4,8 @@
 @net user TempAdmin TA /add
 @net localgroup Administrators TempAdmin /add
 @echo Success! Do what you have to do, then return to this window. DO NOT PRESS ANY BUTTON in this window yet.
-@echo Use password TA
+@echo Use password TA. Password copied to clipboard.
+@@echo TA |clip
 @echo Press any key once you are done with tasks. The account auto deletes in 10 minutes.
 ::If you want, change 600 to a custom time period, in seconds.
 @timeout 600
