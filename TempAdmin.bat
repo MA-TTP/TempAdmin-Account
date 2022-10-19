1e772fda55e4802dcd95c7c84c9c449735692362
MA-TTP <72108630+MA-TTP@users.noreply.github.com>
2022-10-19 15:28:40 -0400

    1.1

--- TempAdmin.bat
+++ TempAdmin.bat
@@ -1,16 +1,18 @@
 @title TempAdmin. GitHub Edition. License: MIT
-@echo  Copyright Mashoor Aiyaan. Version 1.0, build 101722.
+@echo  Copyright Mashoor Aiyaan. Version 1.0, build 101922.
 ::edit out "TA" with any password you like, such as 1234.
 @net user TempAdmin TA /add
 @net localgroup Administrators TempAdmin /add
 @echo Success! Do what you have to do, then return to this window. DO NOT PRESS ANY BUTTON in this window yet.
 @echo Use password TA. Password copied to clipboard.
-@@echo TA|clip
+::match this with the password specified above
+@echo TA|clip
 @echo Press any key once you are done with tasks. The account auto deletes in 10 minutes.
 ::If you want, change 600 to a custom time period, in seconds.
 @timeout 600
 @net user TempAdmin /delete
 cd C:/Windows/Users
 @attrib +h /s /d TempAdmin
-@echo Successfully executed script.
+@echo Successfully executed script. You can now close this window
+@timeout 120
 exit
--- TempAdminInt.bat
+++ TempAdminInt.bat
@@ -4,7 +4,8 @@
 @net user TempAdmin TA /add
 @net localgroup Administrators TempAdmin /add
 @echo Press any key once you are done with tasks. The account auto deletes in 10 minutes.
-::If you want, change 600 to a custom time period, in seconds.
+::If you want, change 600 to a custom time period, in seconds. Password is auto copied to clipboard.
+@echo TA|clip
 @timeout 600
 @net user TempAdmin /delete
 cd C:/Windows/Users
